cf434c2c63cd7f1a5dc7e11bdc3bb36a30807e05
JiLong <skyujilong@163.com>
2017-07-20 15:54:28 +0800

    过滤内容 资源  初始化queue

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 'use strict';
-var colors = require('colors');
-var path = require('path');
-var _ = require('lodash');
-var readDir = require('./src/compression.js').readDir;
-var readFile = require('./src/compression.js').readFile;
-var compressImg = require('./src/compression.js').compressImg;
-var emitImg = require('./src/compression.js').emitImg;
 
+const _ = require('lodash');
 const uploader = require('./src/uploader.js');
-
 
 function TinyPNGPlugin(options) {
     this.options = _.assign({
@@ -29,97 +22,14 @@ function TinyPNGPlugin(options) {
 }
 
 TinyPNGPlugin.prototype.apply = function(compiler) {
-    var _self = this,
-        targetImgDir = this.getImgDir(compiler.options.output.path);
-    //TODO 上传文件操作开始
-    compiler.plugin('emit',(compilation, cb) => {
-        uploader(compilation,).then(() => {
+    //上传文件操作开始
+    compiler.plugin('emit', (compilation, cb) => {
+        uploader(compilation, this.options).then(() => {
             cb();
         }).catch((e) => {
             console.log(e.stack);
             cb();
         });
-        // _.each(compilation.assets,(val,key) => {
-        //     console.log(key);
-        //     val._value = 'test change source'
-        //     //有可能source是一个buffer
-        //     console.log(val.source());
-        //     //console.log(require('util').inspect(val, { depth: null }));
-        // });
-        // console.log(require('util').inspect(compilation.assets, { depth: null }));
-
-    });
-};
-
-
-
-
-
-TinyPNGPlugin.prototype.getImgDir = function(outputPath) {
-        var imgUrls = [];
-        if (_.isString(this.options.relativePath)) {
-
-            imgUrls.push(path.resolve(outputPath, this.options.relativePath));
-
-        } else if (_.isArray(this.options.relativePath)) {
-
-            _.forEach(this.options.relativePath, function(relativePath) {
-                imgUrls.push(path.resolve(outputPath, relativePath));
-            });
-
-        }
-        return imgUrls;
-    }
-    /**
-     * 上传到tinyPNG 官网上面进行文件压缩处理
-     * @param  {Array}   targetImgDir 存储图片的绝对路径
-     * @param  {Object}   compilation  [description]
-     * @param  {Function} callback     [description]
-     * @return {[type]}                [description]
-     */
-TinyPNGPlugin.prototype.upload = function(targetImgDir, compilation, callback) {
-    var imgCount = 0,
-        allCount = 0,
-        _self = this;
-    console.log('\n'); //换行
-    _.forEach(targetImgDir, function(imgDir) {
-        readDir(imgDir).then(function(files) {
-            var promiseList = [];
-            _.forEach(files, function(file) {
-                if (_self.reg.test(file)) {
-                    allCount++;
-                    imgCount++;
-                    promiseList.push(_self.compress(path.join(imgDir, file), function() {
-                        imgCount--;
-                        console.log(colors.yellow('tinyPNG-webpack-plugin: img compress process is ' + (allCount - imgCount) + '/' + allCount));
-                    }).catch(function(e) {
-                        //AccountError 用户认证失败
-                        imgCount--;
-                        console.log(colors.red('tinyPNG-webpack-plugin:' + file + ', compress error'));
-                        compilation.errors.push(e);
-                    }));
-                }
-            });
-            return Promise.all(promiseList);
-        }).then(function() {
-            if (imgCount <= 0) {
-                callback();
-            }
-        }).catch(function(e) {
-            callback()
-            compilation.errors.push(e);
-            throw e;
-        })
-    });
-};
-TinyPNGPlugin.prototype.compress = function(imgDir, cb) {
-    var _self = this;
-    return readFile(imgDir).then(function(ImgFileInfo) {
-        return compressImg(_self.options.key, ImgFileInfo);
-    }).then(function(compressImgInfo) {
-        return emitImg(compressImgInfo);
-    }).then(function() {
-        cb();
     });
 };
 
--- src/uploader.js
+++ src/uploader.js
@@ -3,14 +3,33 @@ const co = require('co');
 const thunkify = require('thunkify');
 const _ = require('lodash');
 
+
+function getImgQueue(list,reg){
+    let queue = [];
+
+    _.each(list,function(val,key){
+        if(reg.exec(key)){
+            //val RawSource 对象
+            queue.push(val);
+        }
+    });
+
+    return queue;
+}
+
+
 /**
  * 进行图片上传主操作
- * @param  {[type]} targetImgDir [description]
- * @param  {[type]} compilation  [description]
- * @return {[type]}              [description]
+ * @param  {[type]} compilation     [webpack 构建对象]
+ * @param  {[type]} options         [选项]
+ * @return {Promise}
  */
-module.exports = (targetImgDir, compilation) => {
+module.exports = (compilation, options) => {
+    //过滤文件尾缀名称
+    let reg = new RegExp("\.(" + options.ext.join('|') + ')$', 'i');
+
     return co(function * () {
+        let imgQueue = getImgQueue(compilation.assets,reg);
         //TODO 获取文件地址
         //TODO 读取文件内容，之后进行上传操作
     });
