5ea7e91f82ac0fe313708d7bc548db753ec4b570
skyujilong <skyujilong@163.com>
2018-03-16 17:49:02 +0800

    add support webpack 4

--- index.js
+++ index.js
@@ -4,48 +4,46 @@ const _ = require('lodash');
 const uploader = require('./src/uploader.js');
 const stdout = require('./src/stdout.js');
 
+class TinyPNGPlugin {
+    constructor(options){
+        debugger;
+        this.pluginName = 'tinypng-webpack-plugin';
+        this.options = _.assign({
+            key: '',
+            ext: ['png', 'jpeg', 'jpg'],
+            proxy:''
+        }, options);
 
-function TinyPNGPlugin(options) {
-    this.options = _.assign({
-        key: '',
-        ext: ['png', 'jpeg', 'jpg'],
-        proxy:''
-    }, options);
-
-    if (!this.options.key) {
-        throw new Error('need tinyPNG key');
-    }
-
-    if (_.isString(this.options.key)) {
-        this.options.key = [this.options.key];
-    }
-
-    if(_.isString(this.options.proxy) && this.options.proxy !== ''){
-        if(this.options.proxy.indexOf('http://') === -1){
-            throw new Error('the proxy must be HTTP proxy!')
+        if (!this.options.key) {
+            throw new Error('need tinyPNG key');
         }
+
+        if (_.isString(this.options.key)) {
+            this.options.key = [this.options.key];
+        }
+
+        if(_.isString(this.options.proxy) && this.options.proxy !== ''){
+            if(this.options.proxy.indexOf('http://') === -1){
+                throw new Error('the proxy must be HTTP proxy!')
+            }
+        }
+
+        //正则表达式筛选图片
+        this.reg = new RegExp("\.(" + this.options.ext.join('|') + ')$', 'i');
     }
-
-    //正则表达式筛选图片
-    this.reg = new RegExp("\.(" + this.options.ext.join('|') + ')$', 'i');
-}
-
-TinyPNGPlugin.prototype.apply = function(compiler) {
-    //上传文件操作开始
-    compiler.plugin('emit', (compilation, cb) => {
-        stdout.render();
-        uploader(compilation, this.options).then((failList) => {
-            stdout.stop();
-            stdout.renderErrorList(failList);
-            cb();
-        }).catch((e) => {
-            stdout.stop();
-            compiler.errors.push(e);
-            cb();
+    apply(compiler){
+        compiler.hooks.emit.tapPromise(this.pluginName,(compilation)=>{
+            stdout.render();
+            return uploader(compilation, this.options).then((failList) => {
+                stdout.stop();
+                stdout.renderErrorList(failList);
+            }).catch((e) => {
+                stdout.stop();
+                compiler.errors.push(e);
+            });
         });
-    });
-};
-
+    }
+}
 
 
 module.exports = TinyPNGPlugin;
